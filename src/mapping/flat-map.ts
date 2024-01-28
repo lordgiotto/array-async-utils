6655edcef120d3a7b0a3592d6fa33ddb0003943d
Lorenzo Zottar <lordgiotto@gmail.com>
2024-01-28 16:22:11 +0100

    asyncFlatMap type improvement

--- src/mapping/flat-map.ts
+++ src/mapping/flat-map.ts
@@ -7,17 +7,17 @@ type AsyncFlatMapCallback<
   element: ElementOf<A>,
   index: number,
   array: A
-) => Promise<ME> | Promise<ReadonlyArray<ME>>;
+) => Promise<ReadonlyArray<ME> | ME>;
 
 async function asyncFlatMap<
   A extends Array<unknown> | ReadonlyArray<unknown>,
   ME,
->(array: A, callback: AsyncFlatMapCallback<A, ME>) {
+>(array: A, callback: AsyncFlatMapCallback<A, ME>): Promise<ME[]> {
   const promisesArray = array.map((el, index) =>
     callback(el as ElementOf<A>, index, array)
   );
   const mappedArray = await Promise.all(promisesArray);
-  const flattenArray = mappedArray.flat(1);
+  const flattenArray = mappedArray.flat(1) as ME[];
   return flattenArray;
 }
 
